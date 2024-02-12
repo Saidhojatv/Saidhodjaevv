2853fbcebd9718fdbf571111e027441035c104cb
GGA777 <grisha19437@yandex.ru>
2024-02-12 22:14:00 +0300

    master

--- app/src/main/java/com/example/saidhodjaev/SecondActivity.java
+++ app/src/main/java/com/example/saidhodjaev/SecondActivity.java
@@ -1,2 +1,35 @@
-package com.example.saidhodjaev;public class SecondActivity {
+package com.example.saidhodjaev;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+        Button button = findViewById(R.id.button5);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this,
+                        ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button button1 = findViewById(R.id.button6);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this,
+                        MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
